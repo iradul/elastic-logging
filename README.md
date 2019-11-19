ed314759d819c1f1e8e99eebbff54bbe4b08003b
Ivan <iradul@gmail.com>
2019-11-18 23:07:53 -0500

    Refactor

--- README.md
+++ README.md
@@ -1,21 +1,50 @@
 # Elastic-Log
-Log directly to Elasticsearch.
+Log directly to Elasticsearch. Library can handle high write rate beacuse it's using bulk API.
 
 ## Example
 
 ```javascript
-const el = new Logging("192.168.1.99:9200");
-const msgExample = {
-    date: (new Date()).getTime(),
-    ip: "192.168.1.102",
-    retries: 8,
-};
-// say today is 01/30/2018
-// this will create index "status-2018-01-30"
-// and add msgExample as document
-el.logt("status", msgExample, { date: { type: "date" } });
+(async function () {
+    const el = new Logger({
+        /**Elasticsearch host */
+        esHost: '192.168.7.120:9200',
+        /**Elasticsearch request timeout in milliseconds. Default is `30000`. */
+        esRequestTimeout_ms: 5000,
+        /**Flush interval in milliseconds. Default is `5000`. */
+        flushInterval_ms: 900,
+        /**Interval in seconds at which new index with suffix gets created.
+         * Suffix is calculated as current timestamp divided by this interval
+         * at the moment of `log` method call.
+         * When set to `0` index will not get any suffix.
+         * 
+         * Default is `3600` (1 hour).*/
+        indexSplitInterval_sec: 60,
+        /**Log errors on stderr. Default is `true`. */
+        logErrors: false,
+    });
 
-// this will create index "test"
-// and add msgExample as document
-el.log("test", msgExample, { date: { type: "date" } });
+    // instance must be initialized
+    await el.initialize();
+
+    const mappingProps = { date: { type: "date" }};
+    const msg1 = {
+        date: Date.now(),
+        ip: "192.168.1.102",
+        retries: 8,
+    };
+    el.log("status", msg1, mappingProps);
+
+    // make sure messages get written to elastic
+    await el.flush();
+
+    const msg2 = {
+        date: Date.now() + 123,
+        ip: "192.168.1.101",
+        retries: 4,
+    };
+    el.log("status", msg2, mappingProps);
+
+    // make sure to gracefully close
+    await el.close();
+})();
 ```
--- index.ts
+++ index.ts
@@ -1,67 +1,124 @@
 import { httpreq } from 'h2tp';
 
-export interface IField {
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-params.html
+interface IBaseFieldMapping {
+    analyzer?: string;
+    boost?: number;
+    coerce?: boolean;
+    copy_to?: string;
+    doc_values?: boolean;
+    dynamically?: boolean | 'strict';
+    enabled?: boolean;
+    format?: string;
+    ignore_above?: number;
+    ignore_malformed?: boolean;
+    index_options?: 'docs' | 'freqs' | 'positions' | 'offsets';
+    index_prefixes?: {
+        min_chars : number;
+        max_chars : number;
+    };
+    index?: boolean;
+    null_value?: any;
+    properties?: IBaseFieldMapping;
+    search_analyzer?: string;
+    similarity?: 'BM25' | 'classic' | 'boolean';
+    store?: boolean;
+    term_vector?: 'no' | 'yes' | 'with_offsets' | 'with_positions' | 'with_positions_offsets' | 'with_positions_payloads' | 'with_positions_offsets_payloads';
     type: "text" | "keyword" | "date" | "long" | "double" | "boolean" | "ip" | "object" | "nested" | "geo_point" | "geo_shape" | "completion";
 }
 
-export type Mappings = {
-    [field: string]: IField
+export interface IFieldProps extends IBaseFieldMapping {
+    fields?: IBaseFieldMapping;
 }
 
-interface ESIndex {
+export type MappingProperties = {
+    [field: string]: IFieldProps;
+}
+
+interface ESBulk {
     bulk: string;
     bulkSize: number;
-    created: number;
+    obsolete: number;
 }
 
 interface ESInfo {
-    name : string;
-    cluster_name : string;
-    cluster_uuid : string;
-    version : {
-      number : string;
-      build_flavor : string;
-      build_type : string;
-      build_hash : string;
-      build_date : string;
-      build_snapshot : boolean;
-      lucene_version : string;
-      minimum_wire_compatibility_version : string;
-      minimum_index_compatibility_version : string;
+    name: string;
+    cluster_name: string;
+    cluster_uuid: string;
+    version: {
+        number: string;
+        build_flavor: string;
+        build_type: string;
+        build_hash: string;
+        build_date: string;
+        build_snapshot: boolean;
+        lucene_version: string;
+        minimum_wire_compatibility_version: string;
+        minimum_index_compatibility_version: string;
     },
-    tagline : string;
-  }
-  
+    tagline: string;
+}
+
+interface ESVersion {
+    major: number;
+    minor: number;
+    patch: number;
+};
+
+export interface Config {
+    /**Elasticsearch host */
+    esHost: string;
+    /**Elasticsearch request timeout in milliseconds. Default is `30000`. */
+    esRequestTimeout_ms?: number;
+    /**Flush interval in milliseconds. Default is `5000`. */
+    flushInterval_ms?: number;
+    /**Interval in seconds at which new index with suffix gets created.
+     * Suffix is calculated as current timestamp divided by this interval
+     * at the moment of `log` method call.
+     * When set to `0` index will not get any suffix.
+     * 
+     * Default is `3600` (1 hour).*/
+    indexSplitInterval_sec?: number;
+    /**Log errors on stderr. Default is `true`. */
+    logErrors?: boolean;
+}
 
 /**
- * Application logging system
- * Logs to Elasticsearch
+ * Elasticsearch bulk logger
  */
-export class Logging {
+export class Logger {
+    public esHost: string;
     public logErrors = true;
-    public elasticBulkInterval = 5000;
-    public elasticRequestTimeout = 4000;
-    public cleanUpInterval = 3600000;
-    private dying = false;
-    private bulkTID: any;
-    private cleanTID: any;
-    private indexes: { [name: string]: ESIndex } = {};
-    private todo: ESIndex[] = [];
-    private esVersion!: { major: number, minor: number, patch: number };
+    public flushInterval = 5000;
+    public esRequestTimeout = 30000;
+    public indexSplitInterval = 3600000;
 
-    constructor(public elasticServer: string) {
-        this.elasticPush();
-        this.cleanUp();
-        const die = () => {
-            this.elasticPush();
-            this.dying = true;
+    private dying = false;
+    private autoFlushTID: any;
+    private cleanUpTID: any;
+    private indexBulks: Map<string, ESBulk> = new Map();
+    private nonEmptyBulks: Set<string> = new Set();
+    private esVersion!: ESVersion;
+    private pendingMessages = 0;
+    private createIndexPromises: Map<string, Promise<ESBulk>> = new Map();
+
+    constructor(config: Config | string) {
+        if (typeof(config) === 'string') {
+            this.esHost = config;
+        } else {
+            this.esHost = config.esHost;
+            this.flushInterval = (config.flushInterval_ms !== undefined) ? config.flushInterval_ms : this.flushInterval;
+            this.esRequestTimeout = (config.esRequestTimeout_ms !== undefined) ? config.esRequestTimeout_ms : this.esRequestTimeout;
+            this.indexSplitInterval = (config.indexSplitInterval_sec !== undefined) ? 1000 * config.indexSplitInterval_sec : this.indexSplitInterval;
+            this.logErrors = (config.logErrors !== undefined) ? !!config.logErrors : this.logErrors;
         }
-        process.on('SIGINT', die);
-        process.on('SIGTERM', die);
+        this.autoFlush();
+        this.cleanUpOldIndexBulks();
     }
 
-    public load() {
-        return httpreq(`http://${this.elasticServer}`)
+    /**Initialize Logger class */
+    public initialize() {
+        return httpreq(`http://${this.esHost}`)
             .then(r => {
                 if (r.response.statusCode === 200) {
                     const info: ESInfo = JSON.parse(r.body);
@@ -74,131 +131,175 @@ export class Logging {
                         }
                         return Promise.resolve();
                     }
-                    return Promise.reject(`Invalid elasticsearch server:\n${JSON.stringify(info, null, 4)}`);
+                    return Promise.reject(new Error(`Invalid elasticsearch server:\n${JSON.stringify(info, null, 4)}`));
                 }
-                return Promise.reject(`Got server contacting elasticsearch server`);
+                return Promise.reject(new Error(`Got server contacting elasticsearch server`));
             })
             .catch(err => {
-                return Promise.reject(`Can't connecto to the server`);
+                this.handleError(err);
+                return Promise.reject(new Error(`Can't connecto to the server`));
             })
     }
 
-    public logt(index: string, message: any, mappings?: Mappings, type = "doc") {
-        return this.log(this.elasticIndexWithTime(index), message, mappings);
-    }
-
-    public log(index: string, message: any, mappings?: Mappings, type = "doc") {
-        if (this.dying) return;
-        const esindex = this.indexes[index];
-        if (!esindex) {
-            const promiseIndex = (!mappings) ? Promise.resolve() : this.createIndex(index, type, JSON.stringify(mappings));
-            promiseIndex.then(() => {
-                const newESindex = {
-                    bulk: '',
-                    bulkSize: 0,
-                    created: (new Date()).getTime(),
-                };
-                this.indexes[index] = newESindex;
-                this.todo.push(newESindex);
-                this.log(index, message, mappings, type);
-            }, (e) => this.handleError(e));
-        } else {
-            esindex.bulk += (this.esVersion.major >= 7)
-                ? `{"index":{"_index":"${index}"}}\n`
-                : `{"index":{"_index":"${index}","_type":"${type}"}}\n`;
-            esindex.bulk += JSON.stringify(message) + '\n';
-            esindex.bulkSize++;
+    /**Close logger gracefully. Instance can't be reused after this call. */
+    public async close(): Promise<void> {
+        this.dying = true;
+        clearTimeout(this.autoFlushTID);
+        clearTimeout(this.cleanUpTID);
+        const delay = 100;
+        while (this.pendingMessages > 0) {
+            await (new Promise(res => setTimeout(() => res(), delay)));
         }
+        return this.flush();
     }
 
-    private elasticIndexWithTime(baseIndex: string) {
-        const today = (new Date()).toISOString().replace(/T.*/, '').replace(/-/g, '.');
-        return `${baseIndex}-${today}`
-    }
-
-    private elasticPush() {
-        clearTimeout(this.bulkTID);
-        let promiseDone: Promise<void>;
-        if (this.todo.length === 0) {
-            promiseDone = Promise.resolve();
+    /**Flush logs to Elasticsearch */
+    public flush() {
+console.log(`size ${this.nonEmptyBulks.size}`)
+        if (this.nonEmptyBulks.size === 0) {
+            return Promise.resolve();
         } else {
             const all: Promise<void>[] = [];
-            this.todo.forEach(esindex => {
-                if (esindex.bulkSize > 0) {
+            this.nonEmptyBulks.forEach(index => {
+                const b = this.indexBulks.get(index);
+                if (b && b.bulkSize > 0) {
+console.log(`doing push ${b.bulkSize}`)
                     all.push(
                         httpreq({
-                            url: `http://${this.elasticServer}/_bulk`,
+                            url: `http://${this.esHost}/_bulk`,
                             method: 'POST',
-                            payload: esindex.bulk,
+                            payload: b.bulk,
                             headers: { "Content-Type": "application/x-ndjson" },
-                            timeout: this.elasticRequestTimeout,
-                        }).then(r => {
-                            return (r.response.statusCode === 200 && (/"errors":(true|false)/.exec(r.body) || '')[1] === "false") ? Promise.resolve() : Promise.reject(new Error(r.body));
+                            timeout: this.esRequestTimeout,
                         })
+                            .then(r => {
+                                return (r.response.statusCode !== 200 || /"errors":true/.exec(r.body)) ? Promise.reject(new Error(r.body || `Got status ${r.response.statusCode} from elasticsearch`)) : Promise.resolve();
+                            })
                     );
-                    esindex.bulk = '';
-                    esindex.bulkSize = 0;
+                    b.bulk = '';
+                    b.bulkSize = 0;
                 }
             });
-            this.todo = [];
-            promiseDone = Promise.all(all).then(() => Promise.resolve());
+            this.nonEmptyBulks.clear();
+            return Promise.all(all).then(() => Promise.resolve());
         }
-        return promiseDone.then(() => {
-            this.bulkTID = setTimeout(() => this.elasticPush(), this.elasticBulkInterval);
-            return Promise.resolve();
-        }, e => {
-            this.handleError(e);
-            this.bulkTID = setTimeout(() => this.elasticPush(), this.elasticBulkInterval);
-            return Promise.resolve();
-        });
     }
 
-    private createIndex(index: string, type: string, properties: string): Promise<void> {
+    /**Log message to elastic */
+    public log(index: string, message: any, properties?: MappingProperties, type = "doc") {
+        if (this.dying) return;
+        if (!this.esVersion) {
+            this.handleError(new Error(`Logger is not initialized`));
+            return;
+        }
+        index = this.elasticIndexWithSufix(index);
+        const b = this.indexBulks.get(index);
+        if (!b) {
+            this.pendingMessages++;
+            this.createIndex(index, type, properties)
+                .then((b2) => {
+                    this.addToBulk(index, b2, message, type);
+                    this.pendingMessages--;
+                })
+                .catch(e => {
+                    this.handleError(e);
+                    this.pendingMessages--;
+                })
+        } else {
+            this.addToBulk(index, b, message, type);
+        }
+    }
+
+    private addToBulk(index: string, b: ESBulk, message: any, type: string) {
+        this.nonEmptyBulks.add(index);
+        b.bulk += (this.esVersion.major >= 7)
+            ? `{"index":{"_index":"${index}"}}\n`
+            : `{"index":{"_index":"${index}","_type":"${type}"}}\n`;
+        b.bulk += JSON.stringify(message) + '\n';
+        b.bulkSize++;
+    }
+
+    private elasticIndexWithSufix(baseIndex: string) {
+        return this.indexSplitInterval === 0 ? baseIndex : `${baseIndex}-${Math.floor(Date.now() / this.indexSplitInterval)}`;
+    }
+
+    private autoFlush() {
+        if (this.autoFlushTID === null) {
+            this.autoFlushTID = setInterval(() => {
+                this.flush();
+                this.autoFlush();
+            }, this.flushInterval);
+        }
+    }
+
+    private createIndex(index: string, type: string, properties?: MappingProperties): Promise<ESBulk> {
+        if (this.createIndexPromises.has(index)) {
+            return this.createIndexPromises.get(index)!;
+        }
         const
-            indexUrl = `http://${this.elasticServer}/${index}`,
+            indexUrl = `http://${this.esHost}/${index}`,
             mappingUrl = (this.esVersion.major >= 7)
                 ? `${indexUrl}/_mapping`
                 : `${indexUrl}/_mapping/${type}`;
+
+        const resolveESBulk = (obsolete?: number): Promise<ESBulk> => {
+            const b: ESBulk = { bulk: '', bulkSize: 0, obsolete: obsolete !== undefined ? obsolete : Date.now() + 2 * this.indexSplitInterval };
+            this.indexBulks.set(index, b);
+            return Promise.resolve(b);
+        };
         // check if type exists
-        return httpreq({
-            url: mappingUrl,
-            method: 'GET',
-            timeout: this.elasticRequestTimeout,
-        }).then((r) => {
-            if (r.response.statusCode === 200) {
-                // index exists
-                return Promise.resolve();
-            } else if (r.response.statusCode === 404) {
-                // index doesn't exist, create it
-                const payload = (this.esVersion.major >= 7)
-                    ? `{"mappings":{"properties":${properties}}}`
-                    : `{"mappings":{"${type}":{"properties":${properties}}}}`;
-                return httpreq({
-                    url: indexUrl,
-                    method: 'PUT',
-                    payload,
-                    headers: { "Content-Type": "application/json" },
-                    timeout: this.elasticRequestTimeout,
-                }).then(r => (r.response.statusCode !== 200) ? Promise.reject("Can't create indice") : Promise.resolve());
-            } else {
-                // unknown error while checking existence
-                return Promise.reject("Can't create indice");
-            }
-        })
+        const promise = !properties
+            ? resolveESBulk()
+            : httpreq({
+                url: mappingUrl,
+                method: 'GET',
+                timeout: this.esRequestTimeout,
+            })
+            .then((r) => {
+                if (r.response.statusCode === 200) {
+                    // index exists
+                    return resolveESBulk(this.indexSplitInterval > 0 ? +/\d+$/.exec(index)![0] * this.indexSplitInterval : undefined);
+                } else if (r.response.statusCode === 404) {
+                    // index doesn't exist, create it
+                    const propsJson = JSON.stringify(properties);
+                    const payload = (this.esVersion.major >= 7)
+                        ? `{"mappings":{"properties":${propsJson}}}`
+                        : `{"mappings":{"${type}":{"properties":${propsJson}}}}`;
+                    return httpreq({
+                        url: indexUrl,
+                        method: 'PUT',
+                        payload,
+                        headers: { "Content-Type": "application/json" },
+                        timeout: this.esRequestTimeout,
+                    })
+                    .then(r => (r.response.statusCode === 200) ? resolveESBulk() : Promise.reject(new Error("Can't create indice")))
+                } else {
+                    // unknown error while checking existence
+                    return Promise.reject(new Error("Can't create indice"));
+                }
+            })
+        promise
+            .catch(() => Promise.resolve())
+            .then(() => this.createIndexPromises.delete(index))
+        this.createIndexPromises.set(index, promise);
+        return promise;
     }
 
-    private cleanUp() {
-        this.cleanTID = setTimeout(() => {
-            this.cleanUp();
-        }, this.cleanUpInterval);
-        const mintime = (new Date()).getTime() - this.cleanUpInterval;
-        for (const p in this.indexes) {
-            const esindex = this.indexes[p];
-            if (esindex.created < mintime) delete this.indexes[p];
+    private cleanUpOldIndexBulks() {
+        if (this.indexSplitInterval > 0) {
+            this.cleanUpTID = setTimeout(() => {
+                this.cleanUpOldIndexBulks();
+            }, this.indexSplitInterval);
+            const mintime = Date.now() - this.indexSplitInterval;
+            for (const entries of this.indexBulks.entries()) {
+                if (entries[1].obsolete < mintime) {
+                    this.indexBulks.delete(entries[0]);
+                }
+            }
         }
     }
 
     private handleError(e: Error) {
-        if (this.logErrors) console.log(`elastic-log: ${e.message}\n${e.stack}`);
+        if (this.logErrors) console.error(`elastic-log: ${e.message}\n${e.stack}`);
     }
 }
